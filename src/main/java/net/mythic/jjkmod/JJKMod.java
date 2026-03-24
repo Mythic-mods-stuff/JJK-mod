3e0d5c723457cb5aea1fdc55baea6a04bf3c046d
Mythic <JannisKu@gmx.de>
2026-03-24 20:39:54 +0100

    Initial Commit

---
+++ src/main/java/net/mythic/jjkmod/JJKMod.java
@@ -0,0 +1,17 @@
+package net.mythic.jjkmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JJKMod implements ModInitializer {
+	public static final String MOD_ID = "jjk-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mythic/jjkmod/JJKModClient.java
@@ -0,0 +1,10 @@
+package net.mythic.jjkmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JJKModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
